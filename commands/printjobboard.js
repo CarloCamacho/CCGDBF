061cb9c3dc2c35e5f7245a9afc8bad7fd7fbfa15
Ian Franklin <43898777+CarloCamacho@users.noreply.github.com>
2020-09-08 15:20:45 +0800

    Update printjobboard.js
    Updated to use collections and a new function to load jobs

--- commands/printjobboard.js
+++ commands/printjobboard.js
@@ -1,52 +1,40 @@
 /*
 This prints the job board ...
 */
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
+var fs = require('fs');
 
-exports.run = (client, message, args, level) => {
 
-const figlet = require('figlet');
+exports.run = async (client, message, args, level) => {
+    
+  // Here we load **jobs** into memory, as a collection, so they're accessible
+  // here and everywhere else.
+  const jobFiles = await readdir("./jobs/");
+  client.logger.log(`Loading a total of ${jobFiles.length} jobs.`);
+  jobFiles.forEach(f => {
+    const response = client.loadJobs(f);
+    if (response) console.log(response);
+    });
 
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    message.channel.send(data)
-});
+  const myJobs = client.jobs
+  const jobNames = myJobs.keyArray();
+  const longest = jobNames.reduce((long, str) => Math.max(long, str.length), 0);
 
-//     // If no specific command is called, show all filtered commands.
-//     if (!args[0]) {
-//       // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
-//       const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
-  
-//       // Here we have to get the command names only, and we use that array to get the longest name.
-//       // This make the help commands "aligned" in the output.
-//       const commandNames = myCommands.keyArray();
-//       const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  
-//       let currentCategory = "";
-//       let output = `= Command List =\n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
-//       const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
-//       sorted.forEach( c => {
-//         const cat = c.help.category.toProperCase();
-//         if (currentCategory !== cat) {
-//           output += `\u200b\n== ${cat} ==\n`;
-//           currentCategory = cat;
-//         }
-//         output += `${message.settings.prefix}${c.help.name}${" ".repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
-//       });
-//       message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
-//     } else {
-//       // Show individual command's help.
-//       let command = args[0];
-//       if (client.commands.has(command)) {
-//         command = client.commands.get(command);
-//         if (level < client.levelCache[command.conf.permLevel]) return;
-//         message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\naliases:: ${command.conf.aliases.join(", ")}\n= ${command.help.name} =`, {code:"asciidoc"});
-//       }
-//     }
+    let currentCategory = "";
+    let output = `= Job List =\n`;
+    const sorted = myJobs.array().sort((p, c) => p.info.category > c.info.category ? 1 :  p.info.name > c.info.name && p.info.category === c.info.category ? 1 : -1 );
+    sorted.forEach( c => {
+      const cat = c.info.category.toProperCase();
+//      if (currentCategory !== cat) {
+//        output += `\u200b\n== ${cat} ==\n`;
+//        currentCategory = cat;
+//      }
+      output += `${c.info.name}${" ".repeat(longest - c.info.name.length)} :: ${c.info.description}\n`;
+    });
+    message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
 };
+
   
   exports.conf = {
     enabled: true,
