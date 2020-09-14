bb1d185c59066481687b45d2c979887949420137
Ian Franklin <43898777+CarloCamacho@users.noreply.github.com>
2020-09-14 22:17:36 +0800

    Removed Perm Levels for now

--- events/message.js
+++ events/message.js
@@ -46,15 +46,15 @@ module.exports = async (client, message) => {
   if (cmd && !message.guild && cmd.conf.guildOnly)
     return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
 
-  if (level < client.levelCache[cmd.conf.permLevel]) {
-    if (settings.systemNotice === "true") {
-      return message.channel.send(`You do not have permission to use this command.
-  Your permission level is ${level} (${client.config.permLevels.find(l => l.level === level).name})
-  This command requires level ${client.levelCache[cmd.conf.permLevel]} (${cmd.conf.permLevel})`);
-    } else {
-      return;
-    }
-  }
+  // if (level < client.levelCache[cmd.conf.permLevel]) {
+  //  if (settings.systemNotice === "true") {
+  //    return message.channel.send(`You do not have permission to use this command.
+  //Your permission level is ${level} (${client.config.permLevels.find(l => l.level === level).name})
+  //This command requires level ${client.levelCache[cmd.conf.permLevel]} (${cmd.conf.permLevel})`);
+  //  } else {
+  //    return;
+  //  }
+  //}
 
   // To simplify message arguments, the author's level is now put on level (not member so it is supported in DMs)
   // The "level" command module argument will be deprecated in the future.
