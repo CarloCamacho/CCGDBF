3c4c59bb33c75e0c1055a931fef2602d756d73a8
Ian Franklin <43898777+CarloCamacho@users.noreply.github.com>
2020-09-18 13:29:09 +0800

    Testing Embed format for Forecast

---
+++ commands/printembedforecast.js
@@ -0,0 +1,88 @@
+const Discord = require('discord.js');
+const axios = require('axios')
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const openweatherapi = client.config.openweatherapi;
+  const forecastEmbed = (
+    sunMin,
+    sunMax,
+    monMin,
+    monMax,
+    tueMin,
+    tueMax,
+    wedMin,
+    wedMax,
+    thuMin,
+    thuMax,
+    friMin,
+    friMax,
+    satMin,
+    satMax
+    
+  ) =>
+    new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(`Hello, ${author}`, profile)
+      .setTitle(`The seven day forecast for Dusk Ville City is:`)
+      .addField(`Monday`, `Min: ${monMin}\u00B0 C`, true)
+      .addField(`\u200b`, `Max: ${monMax}\u00B0 C`, true)
+      .addField(`Tuesday`, `Min: ${tueMin}\u00B0 C`, true)
+      .addField(`\u200b`, `Max: ${tueMax}\u00B0 C`, true)      
+      .setFooter('The Admins', 'https://cdn.discordapp.com/emojis/734211956390756354.png')
+      .setThumbnail(`https://cdn.discordapp.com/attachments/731358750480531467/735120472919900180/IMG_20200717_222537_524.jpg`);
+
+      axios
+          .get(
+              `https://api.openweathermap.org/data/2.5/onecall?lat=39.74&lon=-104.98&exclude=minutely,hourly&units=metric&appid=daa6a7f969e2e3a145196d552939c173`
+          )
+          .then(response => {
+              let apiData = response;
+              let sunMin = Math.ceil(apiData.data.daily[1].temp.min)
+              let sunMax = Math.ceil(apiData.data.daily[1].temp.max)
+              let monMin = Math.ceil(apiData.data.daily[2].temp.min)
+              let monMax = Math.ceil(apiData.data.daily[2].temp.max)
+              let tueMin = Math.ceil(apiData.data.daily[3].temp.min)
+              let tueMax = Math.ceil(apiData.data.daily[3].temp.max)
+              let wedMin = Math.ceil(apiData.data.daily[4].temp.min)
+              let wedMax = Math.ceil(apiData.data.daily[4].temp.max)
+              let thuMin = Math.ceil(apiData.data.daily[5].temp.min)
+              let thuMax = Math.ceil(apiData.data.daily[5].temp.max)
+              let friMin = Math.ceil(apiData.data.daily[6].temp.min)
+              let friMax = Math.ceil(apiData.data.daily[6].temp.max)
+              let satMin = Math.ceil(apiData.data.daily[7].temp.min)
+              let satMax = Math.ceil(apiData.data.daily[7].temp.max)
+              message.channel.send(forecastEmbed(sunMin,sunMax,monMin,monMax,tueMin,tueMax,wedMin,wedMax,thuMin,thuMax,friMin,friMax,satMin,satMax));
+          }
+
+              
+
+// Code formatted version - looks good on Desktop, but not Mobile :(
+
+// message.channel.send(' ',{files: ["https://cdn.discordapp.com/attachments/731358750480531467/735120472919900180/IMG_20200717_222537_524.jpg"]});
+// message.channel.send(`== Weekly Weather for Dusk Ville City ==\n
+// • Sunday       ::     Min: ${sunMin}°C,         Max: ${sunMax}°C
+// • Monday       ::     Min: ${monMin}°C,         Max: ${monMax}°C
+// • Tuesday      ::     Min: ${tueMin}°C,         Max: ${tueMax}°C
+// • Wednesday    ::     Min: ${wedMin}°C,         Max: ${wedMax}°C
+// • Thursday     ::     Min: ${thuMin}°C,         Max: ${thuMax}°C
+// • Friday       ::     Min: ${friMin}°C,         Max: ${friMax}°C
+// • Saturday     ::     Min: ${satMin}°C,         Max: ${satMax}°C`, {code: "asciidoc"});
+              
+
+})
+     
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: "printforecast",
+  category: "Miscelaneous",
+  description: "Print out a 7 Day forecast for Dusk Ville City.",
+  usage: "printforecast"
+};
