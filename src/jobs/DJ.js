b675e7b66ac20ed14a0d895a15d6714d9c55abf0
Ian Franklin <43898777+CarloCamacho@users.noreply.github.com>
2020-09-26 23:56:55 +0800

    Added DJ job

---
+++ src/jobs/DJ.js
@@ -0,0 +1,5 @@
+exports.info = {
+    name: "DJ",
+    category: "Entertainment & the Arts",
+    description: "It's your job to get the patrons dancing. Good luck! Especially when the clientle's ages range from 18 - 1800",
+  };
