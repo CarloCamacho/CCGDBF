44d25a962962235e2cba2bc7ef204a9bbcddcd16
Ian Franklin <43898777+CarloCamacho@users.noreply.github.com>
2020-10-12 22:12:27 +0800

    Rearranged Command Structure, remove Events

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,7 +4,7 @@ module.exports = (client, member) => {
   // Load the guild's settings
   const settings = client.getSettings(member.guild);
 
-  member.roles.add(member.guild.roles.cache.find(role => role.name === "⁣          Other           ⁣"));
-  member.roles.add(member.guild.roles.cache.find(role => role.name === "⁣          Occupation           ⁣"));
+//  member.roles.add(member.guild.roles.cache.find(role => role.name === "⁣          Other           ⁣"));
+//  member.roles.add(member.guild.roles.cache.find(role => role.name === "⁣          Occupation           ⁣"));
 
 };
